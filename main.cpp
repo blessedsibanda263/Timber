01ad7abd62589a37ad8ffc5264eb0d388c3d8f11
Blessed <blessedsibanda263@gmail.com>
2025-01-10 12:25:48 +0200

    Draw game's background

--- main.cpp
+++ main.cpp
@@ -6,6 +6,15 @@ int main()
 {
     VideoMode vm({1920, 1080});
     RenderWindow window(vm, "Timber!!!");
+
+    Texture textureBackground;
+
+    textureBackground.loadFromFile("graphics/background.png");
+
+    Sprite spriteBackground(textureBackground);
+    spriteBackground.setTexture(textureBackground);
+    spriteBackground.setPosition({0, 0});
+
     while (window.isOpen())
     {
         if (Keyboard::isKeyPressed(Keyboard::Key::Escape))
@@ -13,6 +22,7 @@ int main()
             window.close();
         }
         window.clear();
+        window.draw(spriteBackground);
         window.display();
     }
     return 0;
