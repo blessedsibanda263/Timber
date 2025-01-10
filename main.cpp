8d304a1507f547116b15cb357e069a0241f908ab
Blessed <blessedsibanda263@gmail.com>
2025-01-10 12:01:11 +0200

    Setup SFML 3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace sf;
+
+int main()
+{
+    VideoMode vm({1920, 1080});
+    RenderWindow window(vm, "Timber!!!");
+    while (window.isOpen())
+    {
+        if (Keyboard::isKeyPressed(Keyboard::Key::Escape))
+        {
+            window.close();
+        }
+        window.clear();
+        window.display();
+    }
     return 0;
 }
